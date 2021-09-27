3e7211fcbe7b0359eb5af63818847e26e79a0f32
Jaytru1997 <kparaboj@gmail.com>
2021-09-27 03:43:23 -0700

    print elements in list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 
 def safe_print_list(my_list=[], x=0):
     el = 0
-    for i in my_list:
+    for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
             el += 1
